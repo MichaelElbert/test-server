de0d579ce33d9ad651bbaf40af37f8c0ba65761d
Michael Elbert <mrguy@spam4.me>
2016-05-10 19:41:31 -0700

    Create restart.js

---
+++ restart.js
@@ -0,0 +1,10 @@
+var exec = require('child_process').exec;
+function handle(error, stdout, stderr) {
+    exec("git pull", function (error, stdout, stderr) {
+        console.log(stdout);
+        exec("forever start server.js", function(a, b, c){
+            console.log("Server rebooted!")
+        })
+    })
+}
+exec("forever stopall", handle);
