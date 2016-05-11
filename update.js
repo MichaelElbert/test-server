e0232dbd0d1d7b8b7f67ca038ea2536cd2d44d08
Michael Elbert <mrguy@spam4.me>
2016-05-10 19:46:22 -0700

    Update and rename restart.js to update.js

--- update.js
+++ update.js
@@ -3,7 +3,7 @@ function handle(error, stdout, stderr) {
     exec("git pull", function (error, stdout, stderr) {
         console.log(stdout);
         exec("forever start server.js", function(a, b, c){
-            console.log("Server rebooted!")
+            console.log(b + "\nServer updated and rebooted!")
         })
     })
 }
