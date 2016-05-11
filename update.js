eddfca9e4fa6bfd223f75b3fca527373b3522ff2
Michael Elbert <mrguy@spam4.me>
2016-05-10 19:47:27 -0700

    Update update.js

--- update.js
+++ update.js
@@ -3,7 +3,7 @@ function handle(error, stdout, stderr) {
     exec("git pull", function (error, stdout, stderr) {
         console.log(stdout);
         exec("forever start server.js", function(a, b, c){
-            console.log(b + "\nServer updated and rebooted!")
+            console.log("Server updated and rebooted!")
         })
     })
 }
